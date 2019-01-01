43203530f52ee0065ab00f60df88ec7cbba95e22
Corina Pelichowski <corina@danitt.com>
2019-01-01 18:47:24 +1000

    Work in progress

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const fetch = require("node-fetch");
+
+const cheerio = require("cheerio");
+
+
+fetch("https://www.fmylife.com/")
+  .then(function(response) {
+    return response.text();
+  })
+  .then(function(text) {
+    const cheeryBoi = cheerio.load(text);
+    const fmlSubmission = cheeryBoi(".panel-content p a").text();
+    console.log(fmlSubmission);
+  });
